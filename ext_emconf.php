0c7ce8cc600e3eb8368625196690891176c43722
Vladimir Falcon <v.falcon@familie-redlich.de>
2022-01-14 08:19:51 +0100

    Fixed TYPO3 dependency in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '10.4.99',
+                    'typo3' => '10.4.1-10.4.99',
                 ],
             'conflicts' =>
                 [
