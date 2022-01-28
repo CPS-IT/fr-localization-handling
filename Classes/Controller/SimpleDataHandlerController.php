919d10df56cf702483b46daf0efd8020c42168dc
Vladimir Falcon <v.falcon@familie-redlich.de>
2022-01-28 10:34:58 +0100

    Restrict support to TYPO3 10.4

--- Classes/Controller/SimpleDataHandlerController.php
+++ Classes/Controller/SimpleDataHandlerController.php
@@ -18,7 +18,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SimpleDataHandlerController extends \TYPO3\CMS\Backend\Controller\SimpleDataHandlerController
 {
-    public function init(ServerRequestInterface $request): void
+    protected function init(ServerRequestInterface $request): void
     {
         parent::init($request);
 
